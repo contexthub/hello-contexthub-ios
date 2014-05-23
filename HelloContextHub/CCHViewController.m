a35653fb83a2d24bade301785ab0438784776592
Kevin W. Lee <kevinwarrenlee@gmail.com>
2014-05-23 10:31:43 -0500

    code cleanup

--- HelloContextHub/CCHViewController.m
+++ HelloContextHub/CCHViewController.m
@@ -14,30 +14,28 @@
 
 @implementation CCHViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)createDefaultGeofenceTapped:(id)sender{
+- (IBAction)createDefaultGeofenceTapped:(id)sender {
     [self createSampleGeofence];
 }
 
 - (void)createSampleGeofence {
-    
-//    CLLocationCoordinate2D center = CLLocationCoordinate2DMake(95.0, -30.0);
-    CLLocationDistance radius = 1000;
+    CLLocationDistance radius = 50;
     CLCircularRegion *geofence = [[CLCircularRegion alloc] initWithCenter:self.mapView.centerCoordinate radius:radius identifier:@"sample"];
 
     [CCHGeofenceService createGeofence:geofence completion:^(NSDictionary *fenceInfo, NSError *error) {
-        NSLog(@"Fence Info %@", fenceInfo);
+        if (!error) {
+            NSLog(@"Fence Created %@", fenceInfo);
+        }
     }];
 }
 @end
